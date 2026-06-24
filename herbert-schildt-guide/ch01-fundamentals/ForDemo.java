779965a7053d40b840303fb5919710ab9f40ab59
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-06-24 18:28:52 +0200

    Add ForDemo.java program
    - Implements a basic for-loop counting from 0 to 5.
    - Practices loop control variables and console output iteration.

---
+++ herbert-schildt-guide/ch01-fundamentals/ForDemo.java
@@ -0,0 +1,16 @@
+class ForDemo{
+	public static void main(String[] args){
+
+		int counter; // Declares the variable counter
+
+		// Declares the for statement with counter = 0 and increasing the value of counter by 1 till 5
+
+		for(counter = 0; counter <= 5; counter++){
+			System.out.println("This is count " + counter);
+		}
+
+		// When the for ends, shows a message "Done!"
+		
+		System.out.println("Done!");
+	}
+}
