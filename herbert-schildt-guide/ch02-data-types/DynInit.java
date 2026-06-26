5dea1f8e9e7ec842943e7403a59539d16d56bba8
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-06-26 10:37:50 +0200

    Add DynInit class to calculate cylinder volume

---
+++ herbert-schildt-guide/ch02-data-types/DynInit.java
@@ -0,0 +1,16 @@
+class DynInit {
+	public static void main(String[] args){
+
+		// Set radius, height as double with values
+
+		double radius = 4, height = 5;
+
+		// Set volume as double and initializate dynamically at run time
+
+		double volume = 3.1416 * radius * radius * height;
+
+		// Show the volume of a cylinder
+		
+		System.out.println("The volume of the cylinder is " + volume);
+	}
+}
