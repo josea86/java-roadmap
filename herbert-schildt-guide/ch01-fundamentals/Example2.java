b05b95b3b099bf21e16bfc2268186f8864dc8633
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-06-26 10:36:02 +0200

    Added files Example2.java and Example3.java

---
+++ herbert-schildt-guide/ch01-fundamentals/Example2.java
@@ -0,0 +1,15 @@
+class Example2 {
+  public static void main(String[] arg) {
+    int myVar1;
+    int myVar2;
+
+    myVar1 = 1024;
+
+    System.out.println("myVar1 contains " + myVar1);
+
+    myVar2 = myVar1 / 2;
+
+    System.out.print("myVar2 contains myVar1 / 2: ");
+    System.out.println(myVar2);
+  }
+}
