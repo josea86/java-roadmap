ea2aa4fa8e55b33a3736035439cbac8d5286b063
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-06-26 10:38:36 +0200

    Add Echo.java to calculate distance based on sound

---
+++ herbert-schildt-guide/ch02-data-types/Echo.java
@@ -0,0 +1,16 @@
+class Echo{
+	public static void main(String[] args){
+
+		// Set dist (distance) as double
+
+		double dist;
+
+		// Get distance as result of sound to sound velocity by 2
+
+		dist = (10.3 * 1100) / 2;
+
+		// Show result on screen
+
+		System.out.println("The object is " + dist + " feet away.");
+	}
+}
