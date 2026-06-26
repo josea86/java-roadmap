cc6a2654811a5591d74ac5c42edd007f1e82200f
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-06-26 10:42:24 +0200

    Add Sound class to calculate distance to lightning

---
+++ herbert-schildt-guide/ch02-data-types/Sound.java
@@ -0,0 +1,16 @@
+class Sound{
+	public static void main(String[] args){
+
+		// Set distance to the lightning as double
+
+		double dist;
+
+		// Calculate the distance to the lightning as seconds to velocity (sound velocity as 1100 feet / second)
+
+		dist = 7.2 * 1100;
+
+		// Show result on screen
+		
+		System.out.println("The lightning is " + dist + " feet away.");
+	}
+}
