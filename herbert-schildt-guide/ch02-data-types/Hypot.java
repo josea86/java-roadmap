94ace23acdd82441b4e8e3f2481ffdbaea2b253e
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-06-26 10:39:12 +0200

    Add Hypot.java to calculate hypotenuse

---
+++ herbert-schildt-guide/ch02-data-types/Hypot.java
@@ -0,0 +1,21 @@
+class Hypot{
+	public static void main(String[] args){
+		
+		// Set variables x, y, z as double
+
+		double x, y, z;
+
+		// Set values of x, z
+
+		x = 3;
+		y = 4;
+
+		// Solve Pythagoras Theoreme
+
+		z = Math.sqrt(x*x + y*y);
+
+		// Show result on screen
+		
+		System.out.println("Hypotenuse is " + z);
+	}
+}
