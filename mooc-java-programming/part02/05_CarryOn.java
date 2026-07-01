c011661a31719492d43bf66f640a57dd4a580361
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-07-01 19:36:45 +0200

    Add CarryOn class for user input loop

---
+++ mooc-java-programming/part02/05_CarryOn.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CarryOn {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String message = "";
+        
+        while (true){
+            
+            if(message.equals("no")){
+                break;
+            }
+            
+            System.out.println("Shall we carry on?");
+            message = scanner.nextLine();
+            
+        }
+
+    }
+}
