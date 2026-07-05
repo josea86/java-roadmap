37ced1e7d8dbbe6dfc358023c4dfc9a095905049
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-07-05 17:46:13 +0200

    Add 25_Division.java

---
+++ mooc-java-programming/part02/25_Division.java
@@ -0,0 +1,15 @@
+public class Division {
+
+    public static void main(String[] args) {
+        
+        division(3,5);
+      
+    }
+
+    public static void division(int numerator, int denominator){
+        
+        double div = (double) numerator / denominator;
+        System.out.println("" + numerator + " / " + denominator + " = " + div);
+        
+    }
+}
