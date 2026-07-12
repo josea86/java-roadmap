dac2f09ae8228b91259943a94e07d1640e257cda
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-07-12 19:19:44 +0200

    Add FirstAndLast program to print first and last input
    Implement a program to read strings and print the first and last inputs.

---
+++ mooc-java-programming/part03/06_FirstAndLast.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FirstAndLast {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        
+        int i = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+            i++;
+        }
+        
+        i = i - 1;
+        
+        System.out.println(list.get(0));
+        System.out.println(list.get(i));
+    }
+}
