0cd9607f1fd5fc03f7200e154a1a294fab453895
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-07-15 20:41:49 +0200

    Add PrintInRange class to print numbers within a range

---
+++ mooc-java-programming/part03/15_PrintInRange.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class PrintInRange {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
+        
+        System.out.println("The numbers in range [0,5] are: ");
+        printNumbersInRange(numbers, 0, 5);
+    }
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit){
+        
+        for(int i = 0; i < numbers.size(); i++){
+        
+            if(numbers.get(i) < lowerLimit | numbers.get(i) > upperLimit){
+                continue;
+            }else{
+                System.out.println(numbers.get(i));
+            }
+    }
+        
+    }
+    
+}
