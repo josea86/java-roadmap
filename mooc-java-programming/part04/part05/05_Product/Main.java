98809f65f0a01de22b740b3af7f85c9b1103d7a7
Jose A Morales-Martinez <josean.morales86@gmail.com>
2026-08-20 19:16:46 +0200

    Delete mooc-java-programming/part04/part05/05_Product directory

--- mooc-java-programming/part04/part05/05_Product/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        
-        Product product = new Product("Banana", 1.1, 13);
-        product.printProduct();
-      
-    }
-}
